14035840df1d3e1f039a69aa5eea7c6e3e4e7363
leostelon <53221136+leostelon@users.noreply.github.com>
2022-04-06 19:43:13 +0530

    card layout

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -8,20 +8,29 @@ export function Card({ backgroundImage, title, createdBy, price }) {
 				backgroundImage: `url('${backgroundImage}')`,
 			}}
 		>
-			<div className="card-info-container">
-				<div>
-					{/* title */}
-					<p>Space Rex</p>
+			<div className="card-info">
+				<div className="card-info-profileimg"></div>
+				<div className="card-info-creator">
 					{/* created by */}
-					<p>@leostelon</p>
+					<p>Created : @Leostelon</p>
 				</div>
-				<div>
+				<div className="card-info-top">
+					{/* title */}
+					<div className="card-info-title">
+						<p>Space Rex</p>
+					</div>
+				</div>
+				<div className="card-info-bottom">
 					{/* price */}
-					<p>
-						Price <span>6 WRX($ 1234)</span>
-					</p>
+					<div className="card-info-price">
+						<p>
+							Price <span>6 WRX($ 1234)</span>
+						</p>
+					</div>
 					{/* button */}
-					<p>buy</p>
+					<div className="card-info-action">
+						<p>Buy now</p>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/components/collectionContainer/collectionContainer.js
+++ src/components/collectionContainer/collectionContainer.js
@@ -1,11 +1,11 @@
 import { Card } from "../card/card";
 import "./collectionContainer.css";
 
-export function CollectionContainer({ assets }) {
+export function CollectionContainer({ assets, title }) {
 	return (
 		<div className="collectionContainer">
 			{/* Title */}
-			<p className="collection-title">Trending</p>
+			<p className="collection-title">{title}</p>
 
 			<div className="collection-cards-container">
 				<div className="cards-container">
