44f55cec5be07a675be00ff82452922d8e5ec836
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2022-05-10 18:41:57 +0530

    profile redirect

--- src/components/navBar/NavbarComponent.jsx
+++ src/components/navBar/NavbarComponent.jsx
@@ -4,21 +4,27 @@ import { ConnectComponent } from "../ConnectComponent";
 import { SearchComponent } from "../search/search";
 import { CreateComponent } from "../CreateComponent";
 import "./navBar.css";
+import { useNavigate } from "react-router-dom";
 
 export function NavbarComponent() {
 	const authenticated = useSelector((state) => state.authReducer.authenticated);
 	const user = useSelector((state) => state.authReducer.user);
+	const navigate = useNavigate();
 
 	return (
 		<div className="navbar-container">
-			<img src="spriyo.png" alt="logo" height={36} />
+			<div style={{ cursor: "pointer" }} onClick={() => navigate("/")}>
+				<img src="spriyo.png" alt="logo" height={36} />
+			</div>
 			<div className="navbar-actions">
 				{/* search */}
 				<SearchComponent />
 				{authenticated ? <CreateComponent /> : <div></div>}
 				{/* profile */}
 				{authenticated ? (
-					<CircularProfile userImgUrl={user.displayImage} />
+					<div onClick={() => navigate("/profile")}>
+						<CircularProfile userImgUrl={user.displayImage} />
+					</div>
 				) : (
 					<ConnectComponent />
 				)}
