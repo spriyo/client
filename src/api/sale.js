dbdd87aacb538b323535af76fc4eb418b4f8ce72
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2022-05-13 16:58:21 +0530

    sale api

--- src/api/sale.js
+++ src/api/sale.js
@@ -11,4 +11,60 @@ export class SaleHttpService {
 		);
 		return resolved;
 	}
+
+	async createSale(data) {
+		const resolved = await resolve(
+			axios.post(`${WEB_API_BASE_URL}/sales`, data, {
+				headers: {
+					Authorization: `Bearer ${this.token}`,
+					"Content-Type": "application/json",
+				},
+			})
+		);
+		return resolved;
+	}
+
+	async updateSale(auctionId, data) {
+		const resolved = await resolve(
+			axios.patch(`${WEB_API_BASE_URL}/sales/update/${auctionId}`, data, {
+				headers: {
+					Authorization: `Bearer ${this.token}`,
+					"Content-Type": "application/json",
+				},
+			})
+		);
+		return resolved;
+	}
+
+	async cancelSale(auctionId) {
+		const resolved = await resolve(
+			axios.patch(
+				`${WEB_API_BASE_URL}/sales/cancel/${auctionId}`,
+				{},
+				{
+					headers: {
+						Authorization: `Bearer ${this.token}`,
+						"Content-Type": "application/json",
+					},
+				}
+			)
+		);
+		return resolved;
+	}
+
+	async buySale(auctionId) {
+		const resolved = await resolve(
+			axios.patch(
+				`${WEB_API_BASE_URL}/sales/buy/${auctionId}`,
+				{},
+				{
+					headers: {
+						Authorization: `Bearer ${this.token}`,
+						"Content-Type": "application/json",
+					},
+				}
+			)
+		);
+		return resolved;
+	}
 }
