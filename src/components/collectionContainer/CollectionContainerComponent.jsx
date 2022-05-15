7b77dba2128bb63aae968c4d763c15221e2e7146
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2022-05-15 15:27:33 +0530

    Collection container fix

--- src/components/collectionContainer/CollectionContainerComponent.jsx
+++ src/components/collectionContainer/CollectionContainerComponent.jsx
@@ -11,17 +11,27 @@ export function CollectionContainer({ assets, title }) {
 			<p className="collection-title">{title}</p>
 
 			<div className="collection-cards-container">
-				<div className="cards-container">
-					{assets.slice(0, 6).map((el, i) => (
-						<div key={i}>
-							<Box onClick={() => navigate("/asset/" + el._id)}>
-								<CardComponent asset={el} />
-							</Box>
-						</div>
-					))}
-				</div>
+				{assets.length === 0 ? (
+					<Box p={2} fontWeight={500} textAlign={"center"}>
+						Nothing to show here :(
+					</Box>
+				) : (
+					<div className="cards-container">
+						{assets.slice(0, 6).map((el, i) => (
+							<div key={i}>
+								<Box onClick={() => navigate("/asset/" + el._id)}>
+									<CardComponent asset={el} />
+								</Box>
+							</div>
+						))}
+					</div>
+				)}
 			</div>
-			<div className="collection-action">See all</div>
+			{assets.length === 0 ? (
+				""
+			) : (
+				<div className="collection-action">See all</div>
+			)}
 		</div>
 	);
 }
