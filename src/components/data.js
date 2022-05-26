1b0b2bf49db1eb98195a266b43f48886d89b4bfa
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2022-05-26 15:21:05 +0530

    fetch default chain assets

---
+++ src/components/data.js
@@ -0,0 +1,74 @@
+export const data = [
+	{
+		medias: [
+			{
+				path: "https://assets.foundation.app/0xBd7d8a57E409b0991F82FA4386cb82a2A92a2eA7/2/nft.mp4", // Landscape Video
+			},
+		],
+		created_by: {
+			displayName: "leostelon",
+		},
+		name: "Awesome",
+		type: "video",
+	},
+	{
+		medias: [
+			{
+				path: "https://f8n-production-collection-assets.imgix.net/0xBd7d8a57E409b0991F82FA4386cb82a2A92a2eA7/1/nft.png", // Potrait Photo
+			},
+		],
+		created_by: {
+			displayName: "leostelon",
+		},
+		name: "Awesome",
+		type: "image",
+	},
+	{
+		medias: [
+			{
+				path: "https://assets.foundation.app/0xd86ECFAA22fD11eE975967958B537FEA55F382Bd/12/nft.mp4", // Potrait Video
+			},
+		],
+		created_by: {
+			displayName: "leostelon",
+		},
+		name: "Awesome",
+		type: "video",
+	},
+	{
+		medias: [
+			{
+				path: "https://f8n-production-collection-assets.imgix.net/0xF3CF4fE3040225236E224668DeF95e9D3966e131/1/nft.jpg", // Landscape Photo
+			},
+		],
+		created_by: {
+			displayName: "leostelon",
+		},
+		name: "Awesome",
+		type: "image",
+	},
+	{
+		medias: [
+			{
+				path: "https://f8n-production-collection-assets.imgix.net/0xb7BB2Ff4fAdFE337B90A13a59e86f488649151F1/1/nft.jpg", // Square Image
+			},
+		],
+		created_by: {
+			displayName: "leostelon",
+		},
+		name: "Awesome",
+		type: "image",
+	},
+	{
+		medias: [
+			{
+				path: "https://f8n-production-collection-assets.imgix.net/0xd86ECFAA22fD11eE975967958B537FEA55F382Bd/8/nft.jpg", // Potrait Image
+			},
+		],
+		created_by: {
+			displayName: "leostelon",
+		},
+		name: "Awesome",
+		type: "image",
+	},
+];
