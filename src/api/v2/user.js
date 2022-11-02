b7baaa73c65326ed82fab60ad6d3367c653d1136
leostelon <53221136+leostelon@users.noreply.github.com>
2022-11-03 00:35:15 +0530

    skip for getusernfts bug fix

--- src/api/v2/user.js
+++ src/api/v2/user.js
@@ -5,14 +5,17 @@ import { resolve } from "../../utils/resolver";
 export class UserHttpService {
 	token = localStorage.getItem("token");
 
-	async getUserNFTs(userAddress) {
+	async getUserNFTs(userAddress, { limit = 10, skip = 0 }) {
 		const resolved = await resolve(
-			axios.get(`${V2_WEB_API_BASE_URL}/users/nfts/${userAddress}`, {
-				headers: {
-					Authorization: `Bearer ${this.token}`,
-					"Content-Type": "multipart/form-data",
-				},
-			})
+			axios.get(
+				`${V2_WEB_API_BASE_URL}/users/nfts/${userAddress}?&limit=${limit}&skip=${skip}`,
+				{
+					headers: {
+						Authorization: `Bearer ${this.token}`,
+						"Content-Type": "multipart/form-data",
+					},
+				}
+			)
 		);
 		return resolved;
 	}
