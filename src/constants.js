a1bb701b979fd8799f761b8cb02bfbcb5efc9bc5
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2023-02-10 23:25:37 +0530

    added shardeumlib2.1

--- src/constants.js
+++ src/constants.js
@@ -41,6 +41,17 @@ export const ChainsConfig = {
 		marketContract: "0xe3D06f136529B2e969CB99a47244B17c7081f90a",
 		auctionContract: "0xcFC27BC05c79eB9e05D1DB6314a2071c720b926C",
 	},
+	SHARDEUM_LIBERTY_2: {
+		chainId: 8081,
+		chainName: "Shardeum Liberty 2.0",
+		nativeCurrency: { name: "Shardeum", symbol: "SHM", decimals: 18 },
+		rpcUrls: ["https://liberty20.shardeum.org/"],
+		blockExplorerUrls: ["https://explorer-liberty20.shardeum.org/"],
+		nftContract: "0x0Fb01DAc8e2F996651Fb76C39F44fee5c13e587e",
+		nft1155Contract: "0xFD1dA889CeF3a6194FEa6587F836e10a8F0ba7D0",
+		marketContract: "0x1E91870ACacd01424A207b99B3f7f54DC7F5EFB5",
+		auctionContract: "0x3B29b69F87aC0c349aaD04303fC6Ac607312467e",
+	},
 	POLYGON_TESTNET: {
 		chainId: 80001,
 		rpcUrl: "https://matic-mumbai.chainstacklabs.com",
